65e61ac0262c73d2aa6973a77a3563ed0c57b2f4
Filipe Kowalsky <filipe.rodrigues.nogueira@gmail.com>
2025-10-31 12:21:46 +0000

    Rename projectSec folder to lowercase for Vercel

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,6 +1,6 @@
 import { BannerSection } from './section/bannerSec/bannerSection';
 import { AboutMeSection } from './section/aboutSec/aboutMeSection';
-import { Projects } from "./section/projectSec/projectsSection";
+import { Projects } from "./section/projectSec/projectSection";
 import { TechSection } from "./section/TechSec/TechSection";
 
 export const Main = () => {
